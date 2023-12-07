140b4faac2978363ddbfcf11b870d05dd8632ccc
asoltner-iut90 <146073354+asoltner-iut90@users.noreply.github.com>
2023-12-07 11:23:30 +0100

    AudrickCommence

---
+++ Original/LPFilter1.java
@@ -0,0 +1,7 @@
+public class LPFilter1 {
+    public double[] lpFilter(double[] inputSignal, double sampleFreq, double cutoffFreq){
+        double[] result = new double[1];
+        
+        return result;
+    }
+}
---
+++ Original/LPFilter2.java
@@ -0,0 +1,7 @@
+public class LPFilter2 {
+    public double[] lpFilter(double[] inputSignal, double sampleFreq, double cutoffFreq){
+        double[] result = new double[1];
+        
+        return result;
+    }
+}
